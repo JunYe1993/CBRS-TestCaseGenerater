b08cd57d0729232213f24c72744bd22a0af34cea
JunYe1993 <d486250@gmail.com>
2019-12-12 09:48:19 +0800

    add cpi and optional switch

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
      
           tablehtml += '<tbody>';
           $.each(data, function(i, item){
-               if(typeCheck(item) || conditionCheck(item))return;
+               if(testplanCheck(item) || cpiCheck(item) || optionalCheck(item) || conditionCheck(item))return;
                tablehtml += '<tr>';
                tablehtml += '<td><label>'+ item.Id +'</label></td>';
                tablehtml += '<td><label>'+ item.Condition +'</label></td>';
@@ -26,7 +26,7 @@ $(document).ready(function() {
           tablehtml += '</table>';
           return tablehtml;
      }
-     function typeCheck(item){
+     function testplanCheck(item){
           var type = $("input[name=\"testplan\"]:checked").val();
           switch(type){
                case undefined:   
@@ -37,19 +37,42 @@ $(document).ready(function() {
                     return (item.DP == "O") ? false:true;
           }
      }
+     function cpiCheck(item){
+          // return false;
+          var type = $("input[name=\"cpi\"]:checked").val();
+          if(item.Title == "WINNF.FT.C.REG.20")
+               console.log(item.Title + " : " + (type == "X" || item.CPI == "O"))
+          return (type == "X" || item.CPI == "O") ? false : true;
+     }
+     function optionalCheck(item){
+          var response = false
+          var type = $("input[name=\"optional\"]:checked").val();
+          if(item.Condition == "O" && type == "X")response = true;
+          return response;
+     }
      function conditionCheck(item){
           var response = true;
           var conditions = $(".condition");
-          if(item.Condition == "M" || item.Condition == "O")return false;
-          conditions.each(function(){
-               if($(this).prop('checked') == true)
-                    if(item.Condition == $(this).next().html())
-                         response = false;
-          });
+          if(item.Condition == "M" || item.Condition == "O"){
+               response = false;
+          }else{
+               conditions.each(function(){
+                    if($(this).prop('checked') == true)
+                         if(item.Condition == $(this).next().html())
+                              response = false;
+               });
+          }
           return response;
      }
      $("#testcaelist").html(getTestCasesTable());
+
      $("input[name=\"testplan\"]").change(function(){
+          getTestCases();
+     });
+     $("input[name=\"cpi\"]").change(function(){
+          getTestCases();
+     });
+     $("input[name=\"optional\"]").change(function(){
           getTestCases();
      });
      $(".condition").click(function(){
